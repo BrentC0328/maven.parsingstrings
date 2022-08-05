1d51ab51a876496753ff14d318004477b4b33cbf
Brent <BrentC0328@gmail.com>
2022-08-05 18:12:02 -0400

    strings, character

--- src/main/java/com/github/curriculeon/StringParser.java
+++ src/main/java/com/github/curriculeon/StringParser.java
@@ -8,11 +8,21 @@ import java.util.List;
 public class StringParser {
 
     public static Character[] parseCharacters(String input) {
-        return null;
+        char[] splitInput = input.toCharArray();
+        Character[] result = new Character[splitInput.length];
+
+        for (int i = 0; i < splitInput.length; i++){
+            result[i] = splitInput[i];}
+        return result;
     }
 
     public static Integer[] parseIntegers(String input) {
-        return null;
+        char[] splitInput = input.toCharArray();
+        Integer[] result = new Integer[splitInput.length];
+
+        for (int i = 0; i < splitInput.length; i++){
+            result [i] = (int) splitInput[i]; }
+        return result;
     }
 
     public static List<String> toList(String[] input) {
@@ -20,6 +30,16 @@ public class StringParser {
     }
 
     public static String[] parseStrings(String input) {
-        return null;
+        if (input == null) {
+            return null;
+        } else {
+            char[] splitInput = input.toCharArray(); // split input into characters
+            String[] result = new String[splitInput.length]; //make empty String Array
+
+            for (int i = 0; i < splitInput.length; i++) // for loop to place each character into the array in order
+                result[i] = String.valueOf(splitInput[i]);
+
+            return result;
+        }
     }
 }
